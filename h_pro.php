f989dc8b417f43df6dcada48abfd2bc5e28f4f23
mami-sugi <mami.no_suke@icloud.com>
2017-02-07 20:15:35 +0900

    Delete h_pro.php

--- h_pro.php
+++
@@ -1,9 +0,0 @@
-<?php
-mb_language('ja');
-mb_internal_encoding('UTF-8');
-
-require(dirname(__FILE__).'/libs/Smarty.class.php');
-$smarty = new Smarty();
-
-$smarty->template_dir = dirname(__FILE__).'/templates';
-$smarty->compile_dir = dirname(__FILE__).'/templates_c';
